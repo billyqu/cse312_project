09746ad395bf6b3d0ee9888541fe4befa10d0cd9
Justin Variara <jrvariar@buffalo.edu>
2023-04-07 12:23:03 -0400

    Added code to register a user into db

--- website/app.py
+++ website/app.py
@@ -1,14 +1,30 @@
-from flask import Flask, jsonify, request, render_template
-from pymongo import MongoClient
+from flask import Flask, jsonify, request, render_template, redirect, url_for
+import pymongo
 
 app = Flask(__name__)
 
-# client = MongoClient('mongodb://localhost/')
-# db = client["Main"]
+client = pymongo.MongoClient('mongo')
+db = client["main"]
+users_collection = db['users']
 
 @app.route('/')
 def index():
+    return render_template('register.html')
+
+@app.route('/register-account', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = {'username': username, 'password': password}
+
+    users_collection.insert_one(user)
+    # Redirect to index endpoint
     return render_template('index.html')
+
+@app.route('/get-user', methods=["GET"])
+def getUser():
+    user = users_collection.find_one({})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
--- website/functions.js
+++ website/functions.js
@@ -3,4 +3,3 @@
 function welcome() {
 	document.getElementById("paragraph").innerHTML += "<br/>This text was added by JavaScript 😀"
 }
-
