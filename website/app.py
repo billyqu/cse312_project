7f44be1393baf8c5a8921bb64bf3483624686948
Alex Skowronski <76066748+adskowro@users.noreply.github.com>
2023-05-11 12:45:15 -0400

    Update app.py

--- website/app.py
+++ website/app.py
@@ -3,7 +3,7 @@ import pymongo
 import bcrypt
 import html
 
-
+#socketio = SocketIO(app)
 app = Flask(__name__)
 app.secret_key = 'random generated key'
 
@@ -120,4 +120,5 @@ def question():
 
 
 if __name__ == '__main__':
+    #socketio.run(app)
     app.run(host='0.0.0.0', port=8000)
